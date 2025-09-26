e9a78ab3af200cf102efdbd86b9fd872a1f7e0ee
ben <ben76020567@gmail.com>
2025-09-26 22:06:35 +0800

    add env and mongo connection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
 import Fastify from "fastify";
 import { Routes } from "./controller/routes.js";
 import { ServiceFactory } from "./appLayer/service.factory.js";
+import fastifyEnv from "@fastify/env";
+import type { Env } from "./controller/env.js";
+import mongodb from "@fastify/mongodb";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const fastify = Fastify({
   logger: true,
+});
+
+await fastify.register(fastifyEnv, {
+  schema: {
+    type: "object",
+    required: ["MONGO_URL"],
+    properties: {
+      MONGO_URL: {
+        type: "string",
+      },
+    },
+  },
+  dotenv: {
+    path: `${__dirname}/.env.example`,
+  },
+});
+
+const env = fastify.getEnvs<Env>();
+
+fastify.register(mongodb, {
+  url: env.MONGO_URL,
 });
 
 const serviceFactory = new ServiceFactory();
