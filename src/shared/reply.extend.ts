0cdf5b91cd3eb9cb5904e7874105c23c9343ca2a
ben <ben76020567@gmail.com>
2025-12-19 15:07:25 +0800

    upd reply http extend

--- src/shared/reply.extend.ts
+++ src/shared/reply.extend.ts
@@ -5,8 +5,15 @@ import { Err } from "../contract/responses.js";
 declare module "fastify" {
   interface FastifyReply {
     OK<T>(data: T): FastifyReply;
-    Conflict(err: Err): FastifyReply;
+    Created<T>(data: T): FastifyReply;
+    NoContent<T>(data: T): FastifyReply;
+    BadRequest(err: Err): FastifyReply;
     Unauthorized(err: Err): FastifyReply;
+    Forbidden(err: Err): FastifyReply;
+    NotFound(err: Err): FastifyReply;
+    Conflict(err: Err): FastifyReply;
+    Gone(err: Err): FastifyReply;
+    Locked(err: Err): FastifyReply;
   }
 }
 
@@ -15,8 +22,18 @@ export const replyHttpPlugin = fp((instance: FastifyInstance) => {
     return this.status(200).send({ data });
   });
 
-  instance.decorateReply("Conflict", function (this: FastifyReply, err: Err) {
-    return this.status(409).send(err);
+  instance.decorateReply("Created", function <T>(this: FastifyReply, data: T) {
+    return this.status(201).send({ data });
+  });
+
+  instance.decorateReply("NoContent", function <
+    T,
+  >(this: FastifyReply, data: T) {
+    return this.status(204).send({ data });
+  });
+
+  instance.decorateReply("BadRequest", function (this: FastifyReply, err: Err) {
+    return this.status(400).send(err);
   });
 
   instance.decorateReply(
@@ -25,4 +42,24 @@ export const replyHttpPlugin = fp((instance: FastifyInstance) => {
       return this.status(401).send(err);
     },
   );
+
+  instance.decorateReply("Forbidden", function (this: FastifyReply, err: Err) {
+    return this.status(403).send(err);
+  });
+
+  instance.decorateReply("NotFound", function (this: FastifyReply, err: Err) {
+    return this.status(404).send(err);
+  });
+
+  instance.decorateReply("Conflict", function (this: FastifyReply, err: Err) {
+    return this.status(409).send(err);
+  });
+
+  instance.decorateReply("Gone", function (this: FastifyReply, err: Err) {
+    return this.status(410).send(err);
+  });
+
+  instance.decorateReply("Locked", function (this: FastifyReply, err: Err) {
+    return this.status(423).send(err);
+  });
 });
